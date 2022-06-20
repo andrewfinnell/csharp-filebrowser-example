abd16e0ce387633415e5071f4c3e976bf54f42fa
Andrew T. Finnell <andrew.finnell@gmail.com>
2022-06-20 12:08:52 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace OpenFileUseAsArg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileToUseInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File to open";
+            // 
+            // fileToUseInput
+            // 
+            this.fileToUseInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileToUseInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fileToUseInput.Location = new System.Drawing.Point(12, 27);
+            this.fileToUseInput.Name = "fileToUseInput";
+            this.fileToUseInput.Size = new System.Drawing.Size(253, 20);
+            this.fileToUseInput.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(271, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select File...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnFileBrowser);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(271, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Run...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.runNotepad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 124);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fileToUseInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Select a File to Open with Notepad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileToUseInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OpenFileUseAsArg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFileBrowser(object sender, EventArgs e)
+        {
+            // This is a .NET Framework WinForm component to show a dialog.
+            // It has opens to force a filter like *.txt
+
+            var fileDialog = new OpenFileDialog()
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                CheckFileExists = true
+            };
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileToUse = fileDialog.FileName;
+                this.fileToUseInput.Text = fileToUse;                
+            }
+        }
+
+        private void runNotepad_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("notepad.exe");
+            // This can also have other command line arguments such as forcing notepad
+            // to open the file as ASCII instead of UNICODE.
+            // startInfo.Arguments = String.Format("/A %0", this.fileToUseInput.Text);
+
+            // But because there is only one parameter needed, then just set
+            // the filename as the first argument directly.
+            startInfo.Arguments = this.fileToUseInput.Text;
+
+            startInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            Process.Start(startInfo);
+        }
+    }
+}
