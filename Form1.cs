c92b10aaae802cb905c58c188144615b3bebe9c1
Tom Finnell <tom.finnell.pz@renesas.com>
2022-06-20 21:53:16 -0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -40,7 +40,7 @@ namespace OpenFileUseAsArg
             ProcessStartInfo startInfo = new ProcessStartInfo("C:\\Program Files (x86)\\Focused Test Inc\\FTI Studio\\FTIStudio.exe");
             // This can also have other command line arguments such as forcing notepad
             // to open the file as ASCII instead of UNICODE.
-             startInfo.Arguments = String.Format("-program ", this.fileToUseInput.Text);
+             startInfo.Arguments = String.Format("-program %0", this.fileToUseInput.Text);
 
             // But because there is only one parameter needed, then just set
             // the filename as the first argument directly.
